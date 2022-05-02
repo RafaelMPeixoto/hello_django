2494f1b31618c4593076792ae8b96be0aeb792e3
RafaelMPeixoto <rafael_deicide@hotmail.com>
2022-05-02 11:11:22 -0300

    Atualizando views e Urls

--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
+from curses.ascii import HT
+import re
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def hello(request,nome, idade):
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome,idade))
 
-def hello(request, nome, idade):
-    return HttpResponse(f'<h1>Olá {nome} sua idade é {idade} anos!</h1>')
+def calculadora(request, valor1, valor2):
+    resultado1 = float(valor1 + valor2)
+    resultado2 = float(valor1 * valor2)
+    resultado3 = float(valor1 / valor2)
+    resultado4 = float(valor1 - valor2)
+    return HttpResponse(f'<h1>Valor da soma de {valor1} + {valor2} é {resultado1} <p> A multiplicação de {valor1} x {valor2} é {resultado2}</p> <p>A divisão de {valor1} / {valor2} é {resultado3}</p> <p>A subtração de {valor1} - {valor2} é {resultado4}</p> </h1>')
+    
+
+
+
+ 
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from multiprocessing.spawn import import_main_path
 from django.contrib import admin
 from django.urls import path
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>', views.hello)
+    path('hello/<nome>/<int:idade>/', views.hello),
+    path('calculadora/<int:valor1>/<int:valor2>/', views.calculadora)
 ]
